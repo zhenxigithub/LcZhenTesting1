36e6404737230153225028162548c46bb4aeb9a0
zhenxigithub <59557593+zhenxigithub@users.noreply.github.com>
2020-01-07 15:20:41 +0800

    Add files via upload

--- home.js
+++ home.js
@@ -5,3 +5,55 @@ function click1(){
 function click2(){
 		document.getElementById('header').style.color = 'purple';
 }
+
+function fadein(){
+	$("#senku").fadeIn();
+}
+
+function fadeout(){
+	$("#senku").fadeOut();
+}
+
+var time = 1;
+var left = 0;
+var bottom = 0;
+
+document.addEventListener("keydown",event=>{
+
+	var rect = document.getElementById('senku').getBoundingClientRect();
+
+	if (event.keyCode == '39') { //right
+		document.getElementById('header').innerHTML = "you pressed right	keypress : " + time;
+		left += 200
+		document.getElementById('senku').style.left = left + "px";
+
+		time++;
+	}
+
+	if (event.keyCode == '37') {//left
+		document.getElementById('header').innerHTML = "you pressed left	keypress : " + time;;
+		left -=200;
+		document.getElementById('senku').style.left = left + "px";
+
+		time++;
+	}
+
+	if (event.keyCode == '38') {
+		document.getElementById('header').innerHTML = "you pressed down	keypress : " + time;
+		bottom += 200 
+		document.getElementById('senku').style.bottom = bottom + "px";
+
+		time++;
+	}
+
+	if (event.keyCode == '40') {
+
+		document.getElementById('header').innerHTML = "you pressed up	keypress : " + time;
+		bottom -= 200
+		document.getElementById('senku').style.bottom = bottom + "px";
+
+		time++;
+	}
+
+
+});
